932cd31ff4d026d83e2eeea8be9de27c922b8043
Lee-KangChen <nihaifong@gmail.com>
2016-09-29 15:18:50 +0800

    added new category page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :categories, only: [:index, :new, :create]
   
   resources :posts do
     resources :comments
